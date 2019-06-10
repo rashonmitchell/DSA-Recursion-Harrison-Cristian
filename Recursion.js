80bee16465106ba78a07ccf32695eccf47bfd6d9
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-06-10 12:53:12 -0700

    added maze

--- Recursion.js
+++ Recursion.js
@@ -91,3 +91,48 @@
 //     }
 //     return Fibonacci(num-2)+Fibonacci(num-1)
 // }
+let maze = [
+    [' ', ' ', ' ', '*', ' ', ' ', ' '],
+    ['*', '*', ' ', '*', ' ', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', '*', '*', '*', '*', '*', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+let mySmallMaze = [
+    [' ', ' ', ' '],
+    [' ', '*', ' '],
+    [' ', ' ', 'e']
+];
+function Maze(maze, x=0, y=0, visited=[], steps=''){
+    if (maze[y][x]==='e'){
+        return steps
+    }
+     visited.push(`[${y}][${x}]`)
+     if (y+1 <maze.length){
+         if( maze[y+1][x] !=='*' && !visited.includes(`[${y+1}][${x}]`)){
+             console.log(x,y)
+             return Maze(maze, x, y+1, [...visited], steps+'D')
+         }
+     }
+     if (x+1 < maze[y].length){
+         if(maze[y][x+1] !=='*' && !visited.includes(`[${y}][${x+1}]`)){
+            console.log(x,y)
+             return Maze(maze, x+1, y, [...visited], steps+'R')
+         }
+     }
+     if(y-1 >=0){
+         if(maze[y-1][x] !=='*' && !visited.includes(`[${y-1}][${x}]`)){
+            console.log(x,y)
+             return Maze(maze, x, y-1, [...visited], steps+'U')
+         }
+
+     }
+     if(x-1 >= 0){
+        if(maze[y][x-1] !=='*' && !visited.includes(`[${y}][${x-1}]`)) {
+            console.log(x,y)
+            return Maze(maze, x-1, y, [...visited], steps+'L')
+        }
+
+     }
+}
+console.log(Maze(mySmallMaze))
