a3a9fcf2097994dfad57818ec838d73ed97bd11f
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-06-10 08:53:37 -0700

    first question

---
+++ Recursion.js
@@ -0,0 +1,12 @@
+// // input number of sheep.
+// // output number of sheep + String
+// // for each recursive call input is the sheep number -1
+// // for each recursive call output is the number of sheep + string || base string
+// function sheep (number_of_sheep){
+//     if (number_of_sheep<=0){
+//         return console.log('All sheep jumped over the fence')
+//     }
+//     console.log(number_of_sheep+':Another sheep jump over the fence')
+//     sheep(number_of_sheep-1)
+// }
+// sheep(3)
